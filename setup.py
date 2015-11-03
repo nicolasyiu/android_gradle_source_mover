ef53a3dbe4863a877661b9e67643719bb544b808
saxer <15201280641@qq.com>
2015-11-03 18:43:19 +0800

    no message

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 setup(
   name = 'android_gradle_source_mover',
   packages = ['android_gradle_source_mover'], # this must be the same as the name above
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'A Android sources mover for gradle build',
   author = 'mumaoxi',
+  license = 'MIT',
   author_email = '15201280641@qq.com',
   url = 'https://github.com/mumaoxi/android_gradle_source_mover', # use the URL to the github repo
   download_url = 'https://github.com/mumaoxi/android_gradle_source_mover/tarball/0.1', # I'll explain this in a second
